355ebfd4f4316103562aa2a0f29abad1d3158447
Ruplyn <henryjohnsons@gmail.com>
2022-06-03 20:45:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function bionicReading(text) {
 
   const arr1 = letterArr.map((el) => {
     const length = el.length;
-    const boldLen = Math.floor((length / 100) *  by 40);
+    const boldLen = Math.floor((length / 100) * 40);
     let boldLetters = [];
     let notBold = [];
     for (let i = 0; i < el.length; i++) {
