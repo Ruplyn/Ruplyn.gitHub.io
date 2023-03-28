a8cf782bad6342f03feb1431ac1169610a383547
Henry Johnson <henryjohnsons@gmail.com>
2023-03-28 21:30:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ button.addEventListener("click", () => {
 });
 
 function bionicReading(text) {
-  const arr2 = text.split('. ').join('.<br><br/>');
+  const arr2 = text.split('.').join('.<br><br/>');
   const words = arr2.split(" ");
   let output = "";
 
   for (let i = 0; i < words.length; i++) {
     if ((i + 1) % 3 === 0) {
-        //output += words[i] + " &middot ";
-        output += words[i] + " ~ ";
+        output += words[i] + " &middot ";
+        //output += words[i] + " ~ ";
     } else {
         output += words[i] + " ";
     }
