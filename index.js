e505745d2c34fb40d1adc904fa20fca3e40dbf69
Henry Johnson <henryjohnsons@gmail.com>
2023-11-25 08:19:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function bionicReading(text) {
 
   const arr1 = letterArr.map((el) => {
     const length = el.length;
-    const boldLen = Math.floor((length / 100) * 40);
+    const boldLen = Math.floor((length / 100) * 30);
     let boldLetters = [];
     let notBold = [];
     for (let i = 0; i < el.length; i++) {
